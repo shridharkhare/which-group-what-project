833df7f77f4309a53e835a7a825cd140aa6cc3d5
shridharkhare <kshreedhar01622@gmail.com>
2024-10-15 01:10:49 +0530

    refactor: restructure db schema

--- backend/students.py
+++ backend/students.py
@@ -1,16 +1,62 @@
 import streamlit as st
 
+supabase = st.session_state.supabase
 
-def get_students():
-    supabase = st.session_state.supabase
+
+def get_all_students():
     try:
         data = (
             supabase.table("students")
             .select("*")
-            .order("div", desc=False)
-            .order("roll_no", desc=True)
+            .order("student_id", desc=False)
             .execute()
         )
+        return data.data
+    except Exception as e:
+        st.error(f"Failed to fetch students: {e}")
+        return {}
+
+
+def flatten_teams(data):
+    for student in data.data:
+        if student["teams"]:
+            student["team_no"] = student["teams"]["team_no"]
+        else:
+            student["team_no"] = None
+        del student["teams"]
+
+
+@st.cache_data(show_spinner=True)
+def get_student_by_batch(div, batch):
+    try:
+        # If batch is not a number, return all students from the division
+        if not batch.isdigit():
+            data = (
+                supabase.table("students")
+                .select(
+                    "div, roll_no, name, batch, teams!students_team_id_fkey(team_no)"
+                )
+                .match({"div": div})
+                .order("student_id", desc=False)
+                .execute()
+            )
+            flatten_teams(data)
+
+        else:
+            data = (
+                supabase.table("students")
+                .select(
+                    "div",
+                    "roll_no",
+                    "name",
+                    "batch",
+                    "teams!students_team_id_fkey(team_no)",
+                )
+                .match({"div": div, "batch": batch})
+                .order("roll_no", desc=False)
+                .execute()
+            )
+            flatten_teams(data)
     except Exception as e:
         st.error(f"Failed to fetch students: {e}")
         return {}
--- pages/students.py
+++ pages/students.py
@@ -1,19 +1,48 @@
 import streamlit as st
-from backend.students import get_students
-from frontend.utils.sidebar import set_sidebar
-from backend.students import get_students
+from backend.students import get_all_students, get_student_by_batch
+from frontend.sidebar.sidebar import set_sidebar
 
+st.set_page_config(page_title="Students", layout="wide", page_icon=":material/people:")
 set_sidebar()
-st.title("Students in PCE Comp Engg")
+
+st.title(":material/people: Students in SE Comp Engineering")
+
+all, batch = st.tabs(["All Students", "Filter By Div/Batch"])
 
 
 def show_students():
-    students: dict = get_students()
+    students: dict = get_all_students()
+    st.dataframe(students, width=1000, height=500)
+
+
+def show_students_by_batch():
+    st.subheader(" Select Division", divider=True)
+    div = st.radio(
+        "Select Division",
+        ["A", "B"],
+        key="batch_div",
+        horizontal=True,
+        label_visibility="collapsed",
+    )
+
+    st.subheader("Enter The Batch", divider=True)
+    batch = st.radio(
+        "Select Batch",
+        ["Show all batches", "1", "2", "3"],
+        horizontal=True,
+        label_visibility="collapsed",
+    )
+    students: dict = get_student_by_batch(div, batch)
     st.dataframe(students, width=1000, height=500)
 
 
 def main():
-    show_students()
+
+    with all:
+        show_students()
+
+    with batch:
+        show_students_by_batch()
 
 
 main()
