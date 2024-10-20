47e5c041d2a7c656e55aa423eb3deb6c70dd0e8f
shridharkhare <kshreedhar01622@gmail.com>
2024-10-20 22:39:22 +0530

    fix: disable add team page if already in team

--- frontend/sidebar/sidebar.py
+++ frontend/sidebar/sidebar.py
@@ -3,6 +3,7 @@ import json
 
 from frontend.utils.logout_button import logout_button
 from streamlit_extras.switch_page_button import switch_page
+from backend.team import get_my_team_id
 
 
 def set_sidebar():
@@ -13,6 +14,13 @@ def set_sidebar():
 
     if not cookies.ready():
         st.stop()
+
+    if user["user_type"] == "student":
+        team_id = get_my_team_id(user["student_id"])
+        if team_id:
+            is_in_team = True
+        else:
+            is_in_team = False
 
     st.sidebar.title("Which-Group-What-Project")
     st.sidebar.write(f"Welcome, {user['full_name']}!")
@@ -25,9 +33,17 @@ def set_sidebar():
         for key in data:
             if user["user_type"] == key:
                 for item in data[key]:
-                    st.sidebar.page_link(
-                        label=item["name"], page=item["link"], icon=item["icon"]
-                    )
+                    if key == "student" and is_in_team and item["name"] == "Add Team":
+                        st.sidebar.page_link(
+                            label=item["name"],
+                            page=item["link"],
+                            icon=item["icon"],
+                            disabled=True,
+                        )
+                    else:
+                        st.sidebar.page_link(
+                            label=item["name"], page=item["link"], icon=item["icon"]
+                        )
 
     with st.sidebar:
         logout_button(cookies)
